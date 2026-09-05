a03cfd42cb466a412aee9c43f3e97ed6ddfb5b15
Jonathan Costa <jonjcosta@gmail.com>
2026-09-05 15:16:42 -0300

    fix: Add item by selection option, not just on click

--- src/app/features/search-form/search-form.ts
+++ src/app/features/search-form/search-form.ts
@@ -1,7 +1,7 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -56,7 +56,8 @@ export class SearchForm implements OnInit {
     )
   }
 
-  public addItem(item: Item): void {
+  public handleSelectedOption(event: MatAutocompleteSelectedEvent) {
+    const item = event.option.value;
     this.state.add(item);
     this.form.get('name')?.reset();
   }
